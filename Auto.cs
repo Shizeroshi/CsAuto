4c719f756b7a7c0f28cfc8d37be4ac4e94c6c161
WeCryAlone <75624361+WeCryAlone@users.noreply.github.com>
2021-04-26 12:28:12 +0200

    Add files via upload

---
+++ Auto.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cs_hw
+{
+    class Auto
+    {
+        //----------------------------- private fields -----------------------------
+
+        private string _brand;
+        private string _model;
+        private string _engine;
+        private string _color;
+        private int _fuel;
+        private int _maxSpeed;
+        private int _nitroValue;
+
+        //----------------------------- get/set methods string -----------------------------
+
+        public string Brand
+        {
+            get => _brand;
+            set
+            {
+                _brand = value;
+            }
+        }
+        public string Model
+        {
+            get => _model;
+            set
+            {
+                _model = value;
+            }
+        }
+        public string Engine
+        {
+            get => _engine;
+            set
+            {
+                _engine = value;
+            }
+        }
+
+        public string Color
+        {
+            get => _color;
+            set
+            {
+                _color = value;
+            }
+        }
+
+        //----------------------------- get/set methods int -----------------------------
+
+        public int GetFuel()
+        {
+            return _fuel;
+        }
+
+        public void SetFuel(int fuel)
+        {
+            if (fuel < 0)
+            {
+                throw new Exception(message: "It's even less than have no fuel");
+            }
+           else if (fuel > 100)
+            {
+                throw new Exception(message: "U have more than enough fuel");
+            }
+            _fuel = fuel;
+        }
+
+        public int GetSpeed()
+        {
+            return _maxSpeed;
+        }
+        public void SetMaxSpeed(int speed)
+        {
+            if (speed < 0)
+            {
+                throw new Exception(message: "It's even less than have no speed");
+            }
+            else if (speed > 900)
+            {
+                throw new Exception(message: "You car is a rocket? I think not");
+            }
+            _maxSpeed = speed;
+        }
+
+        public int GetNitro()
+        {
+            return _nitroValue;
+        }
+        public void SetNitro(int nitro)
+        {
+            if (nitro < 0)
+            {
+                throw new Exception(message: "It's even less than have no nitro");
+            }
+            else if (nitro > 900)
+            {
+                throw new Exception(message: "U have more than enough nitro");
+            }
+            _nitroValue = nitro;
+        }
+
+        //----------------------------- Constructors -----------------------------
+
+        public Auto(int value)
+        {
+            _fuel = value;
+            _maxSpeed = value;
+            _nitroValue = value;
+        }
+
+        //----------------------------- Static -----------------------------
+
+        static string brand = "BMW";
+        static string model = "M8";
+        static int fuel = 100;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cs_hw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto[] auto = new Auto[7];
+
+            for (int i = 0; i < 1; i++)
+            {
+                auto[i] = new Auto(100);
+
+                Console.Write("Enter name of brand: ");
+                try
+                {
+                    auto[i].Brand = Console.ReadLine();
+                }
+                catch(Exception)
+                {
+                    Console.WriteLine("error");
+                }
+                Console.WriteLine("");
+                Console.Write("Enter name of model: ");
+                try
+                {
+                    auto[i].Model = Console.ReadLine();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("error");
+                }
+                Console.WriteLine("");
+                Console.Write("Enter name of engine: ");
+                try
+                {
+                    auto[i].Engine = Console.ReadLine();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("error");
+                }
+                Console.WriteLine("");
+                Console.Write("Enter value of fuel: ");
+                try
+                {
+                    auto[i].SetFuel(Convert.ToInt32(Console.ReadLine()));
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("error");
+                }
+                Console.WriteLine("");
+                Console.Write("Enter value of max speed: ");
+                try
+                {
+                    auto[i].SetMaxSpeed(Convert.ToInt32(Console.ReadLine()));
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("error");
+                }
+                Console.WriteLine("");
+                Console.Write("Enter value of nitro: ");
+                try
+                {
+                    auto[i].SetNitro(Convert.ToInt32(Console.ReadLine()));
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("error");
+                }
+                Console.WriteLine("");
+                Console.Write("Enter color: ");
+                auto[i].Color = Console.ReadLine();
+
+                if(auto[i].Color == "White" || auto[i].Color == "white") Console.ForegroundColor = ConsoleColor.White;
+                if (auto[i].Color == "Blue" || auto[i].Color == "blue") Console.ForegroundColor = ConsoleColor.Blue;
+                if (auto[i].Color == "Red" || auto[i].Color == "red") Console.ForegroundColor = ConsoleColor.Red;
+                if (auto[i].Color == "Black" || auto[i].Color == "black") Console.ForegroundColor = ConsoleColor.Black;
+                if (auto[i].Color == "Magenta" || auto[i].Color == "magenta") Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("");
+                Console.WriteLine("---------------Car is ready---------------");
+                Console.WriteLine("Car: " + auto[i].Brand + " " + auto[i].Model);
+                Console.WriteLine("Engine: " + auto[i].Engine);
+                Console.WriteLine("Fuel: " + auto[i].GetFuel() + "%");
+                Console.WriteLine("Max speed: " + auto[i].GetSpeed() + " km/h");
+                Console.WriteLine("Nitro: " + auto[i].GetNitro() + "%");
+                Console.ResetColor();
+            }
+            Console.ReadKey();
+        }
+    }
+}
